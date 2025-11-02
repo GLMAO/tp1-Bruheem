b38f2360dbb72428cf7aeba7edef3646831758ed
Ben. Ibrahim <benbeghdad@zohamail.com>
2025-11-02 14:51:26 +0100

    Task (c) done

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,20 +1,44 @@
 package org.emp.gl.core.launcher;
 
-import org.emp.gl.clients.Horloge ;
+import org.emp.gl.clients.Horloge;
+import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
 
 /**
- * Hello world!
- *
+ * Launcher application to test the Observer Pattern
  */
 public class App {
 
     public static void main(String[] args) {
-
+        System.out.println("=== Task (c): Testing Horloge with Observer Pattern ===\n");
+        
         testDuTimeService();
     }
 
     private static void testDuTimeService() {
-        Horloge horloge = new Horloge("Num 1") ;
+        // 1. Instantiate the TimerService first (using abstraction)
+        TimerService timerService = new DummyTimeServiceImpl();
+        
+        // 2. Create multiple Horloge instances and inject the TimerService
+        Horloge horloge1 = new Horloge(timerService, "Horloge 1");
+        Horloge horloge2 = new Horloge(timerService, "Horloge 2");
+        Horloge horloge3 = new Horloge(timerService, "Horloge 3");
+        
+        System.out.println("Three clocks registered to TimerService.\n");
+        System.out.println("Starting timer... Each clock will display time every second:\n");
+        
+        // 3. Start the timer service
+        timerService.notify();
+        
+        // Keep the program running
+        try {
+            // Run for 15 seconds to see the clocks in action
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("\n=== End of Test ===");
     }
 
     public static void clearScreen() {
--- tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
+++ tp-gl-master/timer-service-client/src/main/java/org/emp/gl/clients/Horloge.java
@@ -1,26 +1,72 @@
-package org.emp.gl.clients ; 
+package org.emp.gl.clients;
 
-import org.emp.gl.timer.service.TimerService ; 
+import org.emp.gl.timer.service.TimerService;
+import org.emp.gl.timer.service.TimerChangeListener;
 
-
-public class Horloge {
-
-    String name; 
-    TimerService timerService ; 
-
-
-    public Horloge (String name) {
-        this.name = name ; 
-
-        System.out.println ("Horloge "+name+" initialized!") ;
+/**
+ * Horloge (Clock) class that observes a TimerService
+ * and displays the current time every second
+ */
+public class Horloge implements TimerChangeListener {
+    
+    private final TimerService timerService;
+    private final String name;
+    
+    // Store current time values
+    private int heures = 0;
+    private int minutes = 0;
+    private int secondes = 0;
+    private int dixiemes = 0;
+    
+    /**
+     * Constructor with TimerService dependency injection
+     * @param timerService The timer service to observe (abstraction, not implementation)
+     * @param name Name identifier for this clock instance
+     */
+    public Horloge(TimerService timerService, String name) {
+        this.timerService = timerService;
+        this.name = name;
+        
+        // Subscribe as observer to the TimerService
+        this.timerService.addTimeChangeListener(this);
     }
-
-    public void afficherHeure () {
-        if (timerService != null)
-            System.out.println (name + " affiche " + 
-                                timerService.getHeures() +":"+
-                                timerService.getMinutes()+":"+
-                                timerService.getSecondes()) ;
+    
+    /**
+     * Called when any time property changes (Observer pattern callback)
+     * This method is called by the TimerService for each property change
+     * 
+     * @param prop The property that changed (HEURE_PROP, MINUTE_PROP, SECONDE_PROP, DIXEME_DE_SECONDE_PROP)
+     * @param oldValue The old value of the property
+     * @param newValue The new value of the property
+     */
+    @Override
+    public void propertyChange(String prop, Object oldValue, Object newValue) {
+        // Update the corresponding time component
+        switch (prop) {
+            case HEURE_PROP:
+                heures = (Integer) newValue;
+                break;
+            case MINUTE_PROP:
+                minutes = (Integer) newValue;
+                break;
+            case SECONDE_PROP:
+                secondes = (Integer) newValue;
+                break;
+            case DIXEME_DE_SECONDE_PROP:
+                dixiemes = (Integer) newValue;
+                break;
+        }
+        
+        // Display the time only when seconds change (to avoid too much output)
+        if (prop.equals(SECONDE_PROP)) {
+            afficherHeure();
+        }
     }
-
+    
+    /**
+     * Display the current time in format HH:MM:SS
+     */
+    private void afficherHeure() {
+        System.out.printf("%s - %02d:%02d:%02d%n", name, heures, minutes, secondes);
+    }
 }
